7e2f91be68550259766aa6cd6704d79748eb8066
suhal vemu <vemusuhal@gmail.com>
2023-03-13 15:10:09 +0530

    15/150 : FSSW leetcode_1876.py

---
+++ FixedSlidingWindow/leetcode_1876.py
@@ -0,0 +1,11 @@
+#worked in single run
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+
+        res = 0
+
+        for i in range(len(s)-2):
+            if len(set(s[i:i+3]))==3:
+                res +=1
+
+        return res
